88ec4de5b1ec4d56a0d55db4d32d5ca4951ac05e
Ben Radler <benradler@me.com>
2014-05-31 19:15:42 -0700

    First pass gradient implementation

--- public/scripts/routes/router.js
+++ public/scripts/routes/router.js
@@ -5,6 +5,7 @@ $(function() {
 
   var ColorRouter = Backbone.Router.extend({
     routes: {
+      "gradient/:color": 'setGradientColors',
       "*colors": "setColors"
     },
 
@@ -30,6 +31,48 @@ $(function() {
       _.each(colors, function(color) {
         app.Colors.addFromHex("#" + color);
       });
+    },
+
+    /**
+     * generates and sets a gradient as the current colors
+     */
+    setGradientColors: function(color) {
+      if(color.length === 6) {
+        // generate gradient based on `color`
+        var modifier = (this.shadeColor(color, 20) == color ? -20 : 20);
+        var array = [color];
+        for(var i = 0; i < 4; i++) {
+          array.push(this.shadeColor(array[i], modifier));
+        }
+
+        // use existing setColors function to push colors to the router
+        this.setColors(array.join(','));
+      }
+    },
+
+    /**
+     * shades (lightens/darkens) a hex color,
+     * either without '#' prefix
+     * percent is any signed integer
+     */
+    shadeColor: function(color, percent) {
+      var R = parseInt(color.substring(0, 2), 16);
+      var G = parseInt(color.substring(2, 4), 16);
+      var B = parseInt(color.substring(4, 6), 16);
+
+      R = parseInt(R * (100 + percent) / 100);
+      G = parseInt(G * (100 + percent) / 100);
+      B = parseInt(B * (100 + percent) / 100);
+
+      R = (R < 255) ? R : 255;
+      G = (G < 255) ? G : 255;
+      B = (B < 255) ? B : 255;
+
+      var RR = ((R.toString(16).length === 1) ? '0' + R.toString(16) : R.toString(16));
+      var GG = ((G.toString(16).length === 1) ? '0' + G.toString(16) : G.toString(16));
+      var BB = ((B.toString(16).length === 1) ? '0' + B.toString(16) : B.toString(16));
+
+      return RR + GG + BB;
     },
 
     /**
