b5043d2589eac6e227d7c1aea1969e0b2e3b009b
Ben Radler <benradler@me.com>
2014-06-11 00:02:42 -0700

    Add colors collection spec

---
+++ test/assets/scripts/collections/colors_spec.js
@@ -0,0 +1,9 @@
+describe('Color model', function() {
+
+  describe('global', function() {
+    it('has the global collection defined', function() {
+      expect(app.Colors).toBeDefined();
+    });
+  });
+
+});
