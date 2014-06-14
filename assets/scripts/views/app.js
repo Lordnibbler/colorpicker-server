8dc55fdbaedc7dee991ad393378b99d2d6f75add
Ben Radler <benradler@me.com>
2014-06-13 20:19:46 -0700

    Simplify App.js view

--- assets/scripts/views/app.js
+++ assets/scripts/views/app.js
@@ -1,252 +1,246 @@
 "use strict";
 var app = app || {};
 
-app.SwatchAppView = (function(Backbone, $){
-  // build the view
-  var View = Backbone.View.extend({
-    el: "#appframe",
+app.SwatchAppView = Backbone.View.extend({
 
-    isTouchMove: false,
-    startSaturation: 100,
+  el: "#appframe",
 
-    events: {
-      "click #header-tab": "toggleheader",
-      "click #gradient": "generateGradient",
-      "click #clear": "clearColors",
-      "click #complement": "generateComplementaryColors",
-      "click #huecomplement": "generateHueShiftComplementaryColors",
-      "click #white": "generateWhiteColors"
-    },
+  isTouchMove: false,
+  startSaturation: 100,
 
-    initialize: function() {
-      app.Colors.on("add",    this.addOne, this);
-      app.Colors.on("reset",  this.addAll, this);
-      app.Colors.on("remove", this.layout, this);
+  events: {
+    "click #header-tab": "toggleheader",
+    "click #gradient": "generateGradient",
+    "click #clear": "clearColors",
+    "click #complement": "generateComplementaryColors",
+    "click #huecomplement": "generateHueShiftComplementaryColors",
+    "click #white": "generateWhiteColors"
+  },
 
-      this.editModel = new app.Color({h: 180, s: 100, l: 50});
-      this.editModel.on("change", this.render, this);
+  initialize: function() {
+    app.Colors.on("add",    this.addOne, this);
+    app.Colors.on("reset",  this.addAll, this);
+    app.Colors.on("remove", this.layout, this);
 
-      // ensure touch-enabled devices have saturation === 100 by default
-      this.editModel.color().saturation(100);
+    this.editModel = new app.Color({h: 180, s: 100, l: 50});
+    this.editModel.on("change", this.render, this);
 
-      if('ontouchstart' in document.documentElement) {
-        // if we're on a touch-enabled device
-        this.$("#edit")
-          .bind("touchstart",    _.bind(this.touchstart,    this))
-          .bind("touchmove",     _.bind(this.touchmove,     this))
-          .bind("touchend",      _.bind(this.touchend,      this))
-          .bind("gesturestart",  _.bind(this.gesturestart,  this))
-          .bind("gesturechange", _.bind(this.gesturechange, this));
-      } else {
-        // no touch functionality
-        this.$("#constraints").mousemove(_.bind(this.mousemove, this))
-          .scroll(_.bind(this.scroll, this))
-          .click(_.bind(this.grabColor, this))
-          .scrollTop(2000); // set saturation full by default
-      }
+    // ensure touch-enabled devices have saturation === 100 by default
+    this.editModel.color().saturation(100);
 
-      // reinvoke layout method when window is resized
-      $(window).resize(_.bind(this.layout, this));
-    },
+    if('ontouchstart' in document.documentElement) {
+      // if we're on a touch-enabled device
+      this.$("#edit")
+        .bind("touchstart",    _.bind(this.touchstart,    this))
+        .bind("touchmove",     _.bind(this.touchmove,     this))
+        .bind("touchend",      _.bind(this.touchend,      this))
+        .bind("gesturestart",  _.bind(this.gesturestart,  this))
+        .bind("gesturechange", _.bind(this.gesturechange, this));
+    } else {
+      // no touch functionality
+      this.$("#constraints").mousemove(_.bind(this.mousemove, this))
+        .scroll(_.bind(this.scroll, this))
+        .click(_.bind(this.grabColor, this))
+        .scrollTop(2000); // set saturation full by default
+    }
 
-    /**
-     * update background CSS and #edit DOM text to currently selected color in this.editModel
-     */
-    render: function() {
-      this.$("#edit").css({
-        "background": this.editModel.hslCss()
+    // reinvoke layout method when window is resized
+    $(window).resize(_.bind(this.layout, this));
+  },
+
+  /**
+   * update background CSS and #edit DOM text to currently selected color in this.editModel
+   */
+  render: function() {
+    this.$("#edit").css({
+      "background": this.editModel.hslCss()
+    });
+
+    this.$("#edit h2").html(this.editModel.hexCss());
+  },
+
+  layout: function() {
+    var w = $(window).width(),
+        sliceSize = Math.floor(w / (app.Colors.length + 2));
+
+    this.$('#colors .swatch:not(#edit):not(.destroyed)').each(function(i, el) {
+      $(el).css({
+        left: i * sliceSize,
+        width: sliceSize,
       });
+    });
 
-      this.$("#edit h2").html(this.editModel.hexCss());
-    },
+    this.$("#edit").css({
+      left: app.Colors.length * sliceSize,
+    });
+  },
 
-    layout: function() {
-      var w = $(window).width(),
-          sliceSize = Math.floor(w / (app.Colors.length + 2));
+  addOne: function(color) {
+    var view = new app.ColorView({model: color});
+    this.$("#colors").append(view.render().el);
 
-      this.$('#colors .swatch:not(#edit):not(.destroyed)').each(function(i, el) {
-        $(el).css({
-          left: i * sliceSize,
-          width: sliceSize,
-        });
-      });
+    view.$el.css({
+      left: this.$("#edit").css("left"),
+      width: this.$("#edit").css("width")
+    });
 
-      this.$("#edit").css({
-        left: app.Colors.length * sliceSize,
-      });
-    },
+    // defer the render for a frame
+    setTimeout(_.bind(function() {
+      view.$el.addClass("animating");
+      this.layout();
+    }, this), 0);
+  },
 
-    addOne: function(color) {
-      var view = new app.ColorView({model: color});
-      this.$("#colors").append(view.render().el);
+  addAll: function() {
+    this.$("#colors li:not(#edit)").remove();
+    app.Colors.each(this.addOne, this);
 
-      view.$el.css({
-        left: this.$("#edit").css("left"),
-        width: this.$("#edit").css("width")
-      });
+    if(app.Colors.length === 0) {
+      this.layout();
+    }
+  },
 
-      // defer the render for a frame
-      setTimeout(_.bind(function() {
-        view.$el.addClass("animating");
-        this.layout();
-      }, this), 0);
-    },
+  toggleheader: function(event) {
+    this.$el.toggleClass("show-header");
+  },
 
-    addAll: function() {
-      this.$("#colors li:not(#edit)").remove();
-      app.Colors.each(this.addOne, this);
+  grabColor: function(event) {
+    app.Colors.add({
+      color: new Color(this.editModel.color().rgb())
+    });
+  },
 
-      if(app.Colors.length === 0) {
-        this.layout();
-      }
-    },
+  /**
+   * Converts colors to Halo's `r,g,b,a\n` format
+   * Sets all lights to same color for live-preview
+   */
+  colorToRgbString: function(color) {
+    var rgbColors =
+      color.rgb().r + ',' +
+      color.rgb().g + ',' +
+      color.rgb().b + ',' +
+      color.rgb().a + '\n';
 
-    toggleheader: function(event) {
-      this.$el.toggleClass("show-header");
-    },
+    // TODO: make this a prototype function called .repeat()
+    return rgbColors + rgbColors + rgbColors + rgbColors + rgbColors;
+  },
 
-    grabColor: function(event) {
-      app.Colors.add({
-        color: new Color(this.editModel.color().rgb())
-      });
-    },
+  move: function(px, py) {
+    var editEl = this.$("#edit"),
+        w      = editEl.width(),
+        h      = editEl.height(),
+        x, y, offset, hue, lit, col;
 
-    /**
-     * Converts colors to Halo's `r,g,b,a\n` format
-     * Sets all lights to same color for live-preview
-     */
-    colorToRgbString: function(color) {
-      var rgbColors =
-        color.rgb().r + ',' +
-        color.rgb().g + ',' +
-        color.rgb().b + ',' +
-        color.rgb().a + '\n';
+    offset = editEl.offset();
 
-      // TODO: make this a prototype function called .repeat()
-      return rgbColors + rgbColors + rgbColors + rgbColors + rgbColors;
-    },
+    x = Math.max(0, px - offset.left);
+    y = Math.max(0, py - offset.top);
 
-    move: function(px, py) {
-      var editEl = this.$("#edit"),
-          w      = editEl.width(),
-          h      = editEl.height(),
-          x, y, offset, hue, lit, col;
+    hue = Math.floor(x / w * 360),
+    lit = Math.floor(y / h * 100);
 
-      offset = editEl.offset();
+    this.editModel.color().hue(hue).lightness(lit);
+    this.editModel.trigger("change");
 
-      x = Math.max(0, px - offset.left);
-      y = Math.max(0, py - offset.top);
+    this.colorChanged(this.editModel);
+  },
 
-      hue = Math.floor(x / w * 360),
-      lit = Math.floor(y / h * 100);
+  /**
+   * send our Node.js app the current live color data
+   */
+  colorChanged: function(color) {
+    window.socket.emit('colorChanged', {
+      color: this.colorToRgbString(color)
+    });
+  },
 
-      this.editModel.color().hue(hue).lightness(lit);
-      this.editModel.trigger("change");
+  scroll: function(event) {
+    var col, offset = this.$("#constraints").scrollTop() / 10;
+    offset = Math.max(0, Math.min(100, offset));
+    this.editModel.color().saturation(offset);
+    this.editModel.trigger("change");
+  },
 
-      this.colorChanged(this.editModel);
-    },
+  mousemove: function(event) {
+    this.move(event.pageX, event.pageY);
+  },
 
-    /**
-     * send our Node.js app the current live color data
-     */
-    colorChanged: function(color) {
-      window.socket.emit('colorChanged', {
-        color: this.colorToRgbString(color)
-      });
-    },
+  touchstart: function(event) {
+    event.preventDefault();
+    this.isTouchMoved = false;
+  },
 
-    scroll: function(event) {
-      var col, offset = this.$("#constraints").scrollTop() / 10;
-      offset = Math.max(0, Math.min(100, offset));
+  touchmove: function(event) {
+    this.move(event.originalEvent.touches[0].pageX, event.originalEvent.touches[0].pageY);
+    this.isTouchMoved = true;
+  },
+
+  /**
+   * adds color to app.Colors collection if touch event ends
+   */
+  touchend: function(event) {
+    if(! this.isTouchMoved) {
+      this.grabColor();
+    }
+  },
+
+  /**
+   * sets the startSaturation before our gestureChange
+   * event is fired to alter the saturation of colorpicker
+   */
+  gesturestart: function(event) {
+    event.preventDefault();
+    this.startSaturation = this.editModel.get("s");
+  },
+
+  /**
+   * updates the colorpicker's saturation based on
+   * the touch pinch-to-zoom gesture's scale attr
+   */
+  gesturechange: function(event) {
+    if(event.originalEvent.scale) {
+      var offset = Math.max(0, Math.min(100, this.startSaturation * event.originalEvent.scale));
       this.editModel.color().saturation(offset);
       this.editModel.trigger("change");
-    },
-
-    mousemove: function(event) {
-      this.move(event.pageX, event.pageY);
-    },
-
-    touchstart: function(event) {
-      event.preventDefault();
-      this.isTouchMoved = false;
-    },
-
-    touchmove: function(event) {
-      this.move(event.originalEvent.touches[0].pageX, event.originalEvent.touches[0].pageY);
-      this.isTouchMoved = true;
-    },
-
-    /**
-     * adds color to app.Colors collection if touch event ends
-     */
-    touchend: function(event) {
-      if(! this.isTouchMoved) {
-        this.grabColor();
-      }
-    },
-
-    /**
-     * sets the startSaturation before our gestureChange
-     * event is fired to alter the saturation of colorpicker
-     */
-    gesturestart: function(event) {
-      event.preventDefault();
-      this.startSaturation = this.editModel.get("s");
-    },
-
-    /**
-     * updates the colorpicker's saturation based on
-     * the touch pinch-to-zoom gesture's scale attr
-     */
-    gesturechange: function(event) {
-      if(event.originalEvent.scale) {
-        var offset = Math.max(0, Math.min(100, this.startSaturation * event.originalEvent.scale));
-        this.editModel.color().saturation(offset);
-        this.editModel.trigger("change");
-      }
-    },
-
-    /**
-     * Delegate to the router to generate a gradient
-     * based on the first Color in our app.Colors collection
-     */
-    generateGradient: function(event) {
-      app.Router.setGradientColors(app.Colors.first().hexCss().substring(1), 5);
-      this.toggleheader();
-    },
-
-    clearColors: function(event) {
-      app.Router.clearColors();
-      this.toggleheader();
-    },
-
-    /**
-     * Generates a bitwise complementary color and a ramp between them
-     */
-    generateComplementaryColors: function(event) {
-      app.Router.setComplementaryColors();
-      this.toggleheader();
-    },
-
-    /**
-     * Generates a bitwise complementary color and a hue shift between them
-     */
-    generateHueShiftComplementaryColors: function(event) {
-      app.Router.setHueShiftComplementaryColors();
-      this.toggleheader();
-    },
-
-    /**
-     * Generates all white colors
-     */
-    generateWhiteColors: function(event) {
-      app.Router.setWhiteColors();
-      this.toggleheader();
     }
-  });
+  },
 
-  // revealing module design pattern
-  // so we can start the app in main.js
-  return View;
+  /**
+   * Delegate to the router to generate a gradient
+   * based on the first Color in our app.Colors collection
+   */
+  generateGradient: function(event) {
+    app.Router.setGradientColors(app.Colors.first().hexCss().substring(1), 5);
+    this.toggleheader();
+  },
 
-})(Backbone, jQuery);
+  clearColors: function(event) {
+    app.Router.clearColors();
+    this.toggleheader();
+  },
+
+  /**
+   * Generates a bitwise complementary color and a ramp between them
+   */
+  generateComplementaryColors: function(event) {
+    app.Router.setComplementaryColors();
+    this.toggleheader();
+  },
+
+  /**
+   * Generates a bitwise complementary color and a hue shift between them
+   */
+  generateHueShiftComplementaryColors: function(event) {
+    app.Router.setHueShiftComplementaryColors();
+    this.toggleheader();
+  },
+
+  /**
+   * Generates all white colors
+   */
+  generateWhiteColors: function(event) {
+    app.Router.setWhiteColors();
+    this.toggleheader();
+  }
+
+});
